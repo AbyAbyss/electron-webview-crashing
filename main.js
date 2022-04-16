8b3ff2c251f777dbaa170ad5521769527a7cee26
Aby Saji <abyrocksaji@gmail.com>
2022-04-17 00:58:47 +0530

    - Electron is crashing while rendering google.com in webview

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 700,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      webviewTag: true,
+      enableRemoteModule: false
     }
   })
 
--- preload.js
+++ preload.js
@@ -9,4 +9,53 @@ window.addEventListener('DOMContentLoaded', () => {
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
+
+  const webview = document.querySelector('webview')
+  webview.addEventListener('dom-ready', () => {
+    // webview.openDevTools()
+
+    // webview.exec
+  })
 })
+
+window.crashMe = async () => {
+  const webview = document.querySelector('webview');
+  const indicator = document.querySelector('.indicator')
+  
+  indicator.innerText = "Navigating to https://www.google.com/search?q=electron";
+  await wait();
+  await webview.executeJavaScript(`location.href="https://www.google.com/search?q=electron";`, true).catch(console.log);
+
+  await wait(4000);
+  indicator.innerText = "Running `history.back(-1)`";
+  await wait();
+  await webview.executeJavaScript(`history.back(-1);`, true).catch(console.log);
+}
+
+function wait(timeout=2000) {
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve(true);
+    }, timeout)
+  })
+}
+
+window.onloadFunc = () => {
+  console.log("Calledddd");
+  const webview = document.querySelector('webview')
+  const indicator = document.querySelector('.indicator')
+  
+  const loadstart = () => {
+    // indicator.innerText = ''
+  }
+  
+  const loadstop = async () => {
+    const webview = document.querySelector('webview');
+    const url = await webview.executeJavaScript(`location.href`, true).catch(console.log);
+    
+    indicator.innerText = `Loaded: ${url}`
+  }
+
+  webview.addEventListener('did-start-loading', loadstart)
+  webview.addEventListener('did-stop-loading', loadstop)
+}
